e4759da356aa023bc7c249aca671a06684327a37
richardcod12 <100446038+richardcod12@users.noreply.github.com>
2022-04-28 13:00:06 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+
+    <div>
+      <main>
+         <header>     
+            <div classsName="menu">
+
+       < a href="#">Login</a>
+       < a href="#">Quem somos</a>
+       < a href="#">Inicio</a>
+       < a href="#">Faça o seu pedido</a>
+
+     </div>
+
+
+    </header>
+
+    </main>
+
+    <div className="conteudo">
+      <h2>A melhor massa de pizza de São Paulo<br></br> aqui você não passa vontade</h2>
+    
+    </div>
+
+    
+
+    <div className="footer">
+
+      <h3>Rua Joaquim Almeida Silva, Avenida Grande de laville</h3>
+
+    </div>
+
+    </div>
+   
+  );
+}
+
+export default App;
+
+
